5fc2dd651f811c46e1718951e636bc34deb4aaab
Modmuss50 <modmuss50@gmail.com>
2015-10-05 21:01:44 +0100

    Added basic cable multipart

--- src/main/java/techreborn/addons/TechRebornAddons.java
+++ src/main/java/techreborn/addons/TechRebornAddons.java
@@ -3,6 +3,7 @@ package techreborn.addons;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
@@ -13,8 +14,8 @@ import techreborn.addons.client.GuiHandler;
 import techreborn.addons.farm.FarmTree;
 import techreborn.addons.items.ItemFarmPatten;
 import techreborn.addons.parts.FarmInventoryCable;
+import techreborn.addons.parts.SuperConductorCable;
 import techreborn.addons.tiles.TileFarm;
-import techreborn.api.TechRebornItems;
 import techreborn.partSystem.ModPartRegistry;
 
 @Mod(name = "TechReborn-Addons", modid = "techreborn-addons", version = "@MODVERSION@")
@@ -27,7 +28,13 @@ public class TechRebornAddons {
     public static TechRebornAddons instance;
 
     @Mod.EventHandler
-    public void init(FMLInitializationEvent event){
+    public void preInit(FMLPreInitializationEvent event) {
+        ModPartRegistry.registerPart(new SuperConductorCable());
+        ModPartRegistry.registerPart(new FarmInventoryCable());
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
         farm = new BlockFarm();
         GameRegistry.registerBlock(farm, "techreborn.farm");
         GameRegistry.registerTileEntity(TileFarm.class, "TileFarmTR");
@@ -39,12 +46,8 @@ public class TechRebornAddons {
     }
 
     @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent event){
-        System.out.println(TechRebornItems.getItem("gems").getUnlocalizedName());
-
+    public void postInit(FMLPostInitializationEvent event) {
         FarmTree.harvestableLogs.add(Blocks.log);
         FarmTree.harvestableLogs.add(Blocks.log2);
-
-        ModPartRegistry.registerPart(new FarmInventoryCable());
     }
 }
